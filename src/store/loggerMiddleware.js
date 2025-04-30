23262bc0911c04d04b9cff2abcdef3b9a300e388
Swati Bhatia <swatibhatia626@gmail.com>
2025-04-30 16:30:58 +0530

    added loggermiddleware for testing

--- src/store/loggerMiddleware.js
+++ src/store/loggerMiddleware.js
@@ -1,9 +1,15 @@
-// loggerMiddleware.ts
-const loggerMiddleware = store => next => action => {
-    console.log('[Dispatching Action]:', action);
-    const result = next(action);
-    console.log('[New State]:', store.getState());
-    return result;
-  };
-  
-  export default loggerMiddleware;
+const loggerMiddleware = (store) => (next) => (action) => {
+  if (process.env.NODE_ENV === "development") {
+    console.log("[Dispatching Action]:", action)
+  }
+
+  const result = next(action)
+
+  if (process.env.NODE_ENV === "development") {
+    console.log("[New State]:", store.getState())
+  }
+
+  return result
+}
+
+export default loggerMiddleware
